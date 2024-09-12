2573217411a51085f9dccaab5eeb91c739f3e8c6
Thom Mondeaux <Thommond554@gmail.com>
2024-09-12 17:06:03 -0400

    Added a constructor to the VendingMachine class.

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,3 +1,19 @@
 public class VendingMachine {
-    
+
+    private VendingItem chips;
+    private VendingItem cookies;
+    private VendingItem candyBar;
+    private VendingItem gummies;
+
+    private double balance;
+
+    public VendingMachine() {
+        this.balance = 0.0;
+        this.chips = new VendingItem("Chips", 1.75);
+        this.cookies = new VendingItem("Cookies", 2.00);
+        this. candyBar = new VendingItem("Candy Bar", 2.25);
+        this.gummies =  new VendingItem("Gummies", 2.50);
+    }
+
+
 }

41a849f78c9b0c679e396e72d28f8c5f84bd9d23
Thom Mondeaux <Thommond554@gmail.com>
2024-09-12 16:57:35 -0400

    Created VendingItem class details and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+### Vending Machine OOP App
+
+In this assginment I will be implementing the same functionality as the [Vending machine app](https://github.com/Thommond/JavaMenuApp).
+
+The difference is instead of one main program this will follow the concepts of OOP (Object oriented programming) as the name suggests.
--- VendingItem.java
+++ VendingItem.java
@@ -1,3 +1,27 @@
 public class VendingItem {
-    
+    private String itemName;
+    private double itemPrice;
+
+    public VendingItem(String itemName, double itemPrice) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+    }
+
+    // Getter and Setter for itemName
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    // Getter and Setter for itemPrice
+    public double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
 }
